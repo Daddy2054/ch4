d4a73378bd18eb47eb6508b2007985172a9b9083
Jean <acalto2@gmail.com>
2022-12-18 00:57:39 +0100

    bottom sheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,13 +1,25 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
   final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
   NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text.isEmpty ? '0' :amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,33 +31,27 @@ class NewTransaction extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             TextField(
-              // /*           onChanged: (value) {
-              //             titleInput = value;
-              //           }, */
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
             ),
             TextField(
-              //  /*        onChanged: (value) {
-              //           amountInput = value;
-              //         }, */
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (val) => amountInput = val,
             ),
             TextButton(
-              onPressed: (() {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              }),
               child: Text(
-                'Add transaction',
-                style: TextStyle(
-                  color: Colors.purple,
-                ),
+                'Add Transaction',
+                style: TextStyle(color: Colors.purple),
               ),
-            )
+              onPressed: submitData,
+            ),
           ],
         ),
       ),
--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -5,7 +5,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/models/transaction.dart';
 import 'package:flutter_complete_guide/widgets/new_transaction.dart';
-import 'package:flutter_complete_guide/widgets/trasaction_list.dart';
+import 'package:flutter_complete_guide/widgets/transaction_list.dart';
 
 class UserTransactions extends StatefulWidget {
   const UserTransactions({Key key}) : super(key: key);
@@ -15,33 +15,13 @@ class UserTransactions extends StatefulWidget {
 }
 
 class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> _userTransactions = [
-    //  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 16.53,
-        date: DateTime.now()),
-  ];
 
-  void _addNewTransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-      title: txTitle,
-      amount: txAmount,
-      date: DateTime.now(),
-      id: DateTime.now().toString(),
-    );
-    setState(() {
-      _userTransactions.add(newTx);
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[NewTransaction(_addNewTransaction), TransactionList(_userTransactions)],
+  //    children: <Widget>[NewTransaction(_addNewTransaction), ],
     );
   }
-}
+
